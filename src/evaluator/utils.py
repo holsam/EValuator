c3f1d43f5ffce2c90e67cdcf6f959d5000390af5
Sam <hol_sam@icloud.com>
2026-03-23 09:08:22 +0000

    feat(utils): add utility functions
    Updated utils.py to add utility functions _read_mrc_file() and
    _label_components() which are used by several commands.

--- src/evaluator/utils.py
+++ src/evaluator/utils.py
@@ -5,10 +5,16 @@ EValuator: UTILITY FUNCTIONS
 '''
 
 # ====================
-# Import dependencies
+# Import external dependencies
 # ====================
+import logging, mrcfile, numpy, sys
+from pathlib import Path
 from rich import print
-import logging, sys
+from scipy import ndimage
+
+# ====================
+# Import internal dependencies
+# ====================
 from .main import lg
 
 # ====================
@@ -18,11 +24,45 @@ def _init_evaluator():
     # Print top-level splash
     print(f"\n[bold]EValuator[/bold] :microscope-text:")
     print(f"A command line tool for automated morphological analysis and visualisation of extracellular vesicles (EVs) from cryo-electron tomography (cryo-ET) data.")
-    
+
+# ====================
+# Define function: _validate_mrc_file
+# ====================
+def _validate_mrc_file(path: Path):
+    '''
+    Use mrcfile package's built-in validate function to confirm file can be read.
+    '''
+    if not mrcfile.validate(path):
+        lg.warning(f"{path.name} is not a valid MRC file - skipping.")
+        return False
+    else:
+        return True
+
 # ====================
 # Define function: _read_mrc_file
 # ====================
+def _read_mrc_file(path: Path):
+    '''
+    Read an MRC file and return the data array and voxel size in nanometres. If no voxel size is encoded in header, returns None instead.
+    '''
+    with mrcfile.open(str(path), mode='r', permissive=True) as file:
+        data = file.data.copy()
+        vox_a = float(file.voxel_size.x)
+    if vox_a == 0.0:
+        lg.warning(f"{path.name}: voxel size not found in MRC header. Physical measurement units will be voxels.")
+        voxel_size_nm = None
+    else:
+        voxel_size_nm = vox_a / 10.0
+    return data, voxel_size_nm
+
 
 # ====================
 # Define function: _label_components
 # ====================
+def _label_components(binary_vol: numpy.ndarray):
+    '''
+    Labels connected components in binary volumes using full 3D (26) connectivity 
+    '''
+    struc = ndimage.generate_binary_structure(3, 3)
+    components, n_components = ndimage.label(binary_vol, structure=struc)
+    return components, n_components
