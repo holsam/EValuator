e3f0b1002c37beb2afc9b5decf8953d1f848b923
Sam Holland <hol_sam@icloud.com>
2026-06-28 18:39:42 +0100

    refactor(config): update config format
    - Modified config.toml to match command usage.
    - Modified utils/config.py to ensure schema matches config.toml.

--- src/evaluator/utils/config.py
+++ src/evaluator/utils/config.py
@@ -28,15 +28,15 @@ class LogConfig(_Section):
 
 class LabelConfig(_Section):
     '''Configuration parameters for `evaluator label` command'''
-    minimum_diameter: float = Field(..., description='Minimum component diameter (nm)')
-    maximum_diameter: float = Field(..., description='Maximum component diameter (nm)')
-    fill_threshold: float = Field(..., description='Fill ratio threshold for labelling')
-    membrane_thickness_nm: float = Field(..., description='Membrane thickness (nm)')
+    pass
 
 
 class AnalyseConfig(_Section):
     '''Configuration parameters for `evaluator analyse` command'''
-    pass
+    minimum_diameter: float = Field(..., description='Minimum component diameter (nm)')
+    maximum_diameter: float = Field(..., description='Maximum component diameter (nm)')
+    fill_threshold: float = Field(..., description='Fill ratio threshold for labelling')
+    membrane_thickness_nm: float = Field(..., description='Membrane thickness (nm)')
 
 
 class VisualiseConfig(_Section):
