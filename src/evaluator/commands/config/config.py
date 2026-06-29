704b07a7ebfdc4be16f31d5f771d13fd5cd3e999
Sam Holland <hol_sam@icloud.com>
2026-06-29 09:37:21 +0100

    refactor(config): update import statement
    - Modified commands/config/config.py to update import statement to match
    other commands.

--- src/evaluator/commands/config/config.py
+++ src/evaluator/commands/config/config.py
@@ -16,7 +16,7 @@ from typing import Annotated
 # ====================
 # Import EValuator utilities
 # ====================
-from evaluator.utils import config
+from evaluator.utils import config as confutil
 
 # ====================
 # Import internal command utilities
@@ -27,12 +27,12 @@ from evaluator.commands.config.utils import edit
 # Define config command logic function
 # ====================
 def run_config(path: Path, *, stepwise: bool) -> None:
-    resolved = config.resolve_config(path)
+    resolved = confutil.resolve_config(path)
     if resolved.existed:
         print(f'Editing configuration file: [cyan]{resolved.config_path}[/cyan]')
         edit.edit_config(resolved.config_path, stepwise=stepwise)
         return
-    config.create_default_config(resolved.config_path)
+    confutil.create_default_config(resolved.config_path)
     print(f'Created configuration file from defaults at: [cyan]{resolved.config_path}[/cyan]')
     if typer.confirm(text='Edit configuration file now', default=True, prompt_suffix='? '):
         edit.edit_config(resolved.config_path, stepwise=stepwise)
