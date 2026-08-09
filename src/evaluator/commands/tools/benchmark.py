0b6e4b2ff97268790a88c70bf29d65b12caf39f7
Sam Holland <hol_sam@icloud.com>
2026-08-09 14:07:13 +0100

    feat(benchmark): add CLI options
    - Modified evaluator/commands/tools/benchmark.py to add CLI options for
    input/output directories.

--- src/evaluator/commands/tools/benchmark.py
+++ src/evaluator/commands/tools/benchmark.py
@@ -8,6 +8,8 @@ EValuator: BENCHMARKING UTILITIES
 # Import external dependencies
 # ====================
 import typer
+from pathlib import Path
+from typing import Annotated, Optional
 
 # ====================
 # Initialise typer as evaluatorAnimate
@@ -21,7 +23,16 @@ evaluatorBenchmark = typer.Typer(
 # Define command: animate
 # ====================
 @evaluatorBenchmark.command('missing-wedge')
-def benchmark_missing_wedge():
+def benchmark_missing_wedge(
+    input_dir: Annotated[
+        Optional[Path],
+        typer.Option('-i', '--input-dir', help='Directory to use during benchmarking')
+    ] = None,
+    output_dir: Annotated[
+        Path,
+        typer.Option('-o', '--output-dir', help='Output directory for benchmark results', show_default=False)
+    ] = Path('./out/benchmark'),
+):
     '''
     Run missing-wedge correction/fitting accuracy benchmark
     '''
